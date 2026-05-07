aea3f4f502b069a84c5a3201397922794832c66e
serhiy-petryk <sergei_petrik@yahoo.com>
2026-05-07 16:05:45 +0300

    Save original code

---
+++ PredictStockPrice/MLNETStock/ItemStockOriginal.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.ML.Runtime.Api;
+
+namespace MLNETStock
+{
+    public class ItemStockOriginal
+    {
+        [Column("0")]
+        public string ItemID;
+
+        [Column("1")]
+        public float Loccode;
+
+        [Column("2")]
+        public float InQty;
+
+        [Column("3")]
+        public float OutQty;
+
+        [Column("4")]
+        public string ItemType;
+
+        [Column("5")]
+        public float TotalStockQty;
+    }
+
+    public class itemStockQtyPredictionOriginal
+    {
+        [ColumnName("Score")]
+        public float TotalStockQty;
+    }
+}
