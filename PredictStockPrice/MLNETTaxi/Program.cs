99e0a451da33cc7b1d97f0efe166dde0842f0ea8
serhiy-petryk <sergei_petrik@yahoo.com>
2026-05-13 11:53:40 +0300

    x

---
+++ PredictStockPrice/MLNETTaxi/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLNETTaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Predicted Stock: {0}, actual Stock Qty: 4800 ");
+            //Console.WriteLine("Predicted Stock: {0}, actual Stock Qty: 4800  - > Round Predicted Value -> ", Math.Round(prediction.TotalStockQty));
+            Console.ReadLine();
+        }
+    }
+}
