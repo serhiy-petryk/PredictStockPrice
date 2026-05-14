ffae547772e414f9b17f29458c7c362a6d256b0a
serhiy-petryk <sergei_petrik@yahoo.com>
2026-05-14 22:14:43 +0300

    Update Program.cs

--- PredictStockPrice/MLNETTaxi/Program.cs
+++ PredictStockPrice/MLNETTaxi/Program.cs
@@ -16,6 +16,7 @@ namespace MLNETTaxi
             MLContext mlContext = new MLContext(seed: 0);
             var model = Train(mlContext, _trainDataPath);
             Evaluate(mlContext, model, _testDataPath);
+            TestSinglePrediction(mlContext, model);
 
             Console.ReadLine();
         }
@@ -36,6 +37,18 @@ namespace MLNETTaxi
                 .Append(mlContext.Transforms.Concatenate("Features", "VendorIdEncoded", "RateCodeEncoded", "PassengerCount", "TripDistance", "PaymentTypeEncoded"))
                 // Choose a learning algorithm
                 .Append(mlContext.Regression.Trainers.FastTree());
+
+            var x1 = mlContext.Transforms.CopyColumns(outputColumnName: "Label", inputColumnName: "FareAmount");
+            var x2 = x1.Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "VendorIdEncoded",
+                inputColumnName: "VendorId"));
+            var x3 = x2.Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "RateCodeEncoded",
+                inputColumnName: "RateCode"));
+            var x4 = x3.Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "PaymentTypeEncoded",
+                inputColumnName: "PaymentType"));
+            var x5 = x4.Append(mlContext.Transforms.Concatenate("Features", "VendorIdEncoded", "RateCodeEncoded",
+                "PassengerCount", "TripDistance", "PaymentTypeEncoded"));
+            // Choose a learning algorithm
+            var x6 = x5.Append(mlContext.Regression.Trainers.FastTree());
 
             // Train the model
             var model = pipeline.Fit(dataView);
@@ -62,6 +75,31 @@ namespace MLNETTaxi
 
             Console.WriteLine($"*       RSquared Score:      {metrics.RSquared:0.##}");
             Console.WriteLine($"*       Root Mean Squared Error:      {metrics.RootMeanSquaredError:0.##}");
+        }
+
+        static void TestSinglePrediction(MLContext mlContext, ITransformer model)
+        {
+            // Creates a single comment of test data.
+            // Predicts fare amount based on test data.
+            // Combines test data and predictions for reporting.
+            // Displays the predicted results.
+
+            var predictionFunction = mlContext.Model.CreatePredictionEngine<TaxiTrip, TaxiTripFarePrediction>(model);
+            var taxiTripSample = new TaxiTrip()
+            {
+                VendorId = "VTS",
+                RateCode = "1",
+                PassengerCount = 1,
+                TripTime = 1140,
+                TripDistance = 3.75f,
+                PaymentType = "CRD",
+                FareAmount = 0 // To predict. Actual/Observed = 15.5
+            };
+            var prediction = predictionFunction.Predict(taxiTripSample);
+
+            Console.WriteLine($"**********************************************************************");
+            Console.WriteLine($"Predicted fare: {prediction.FareAmount:0.####}, actual fare: 15.5");
+            Console.WriteLine($"**********************************************************************");
         }
     }
 }
