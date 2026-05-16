e665214965fe2cafcf90ffcda1982e12b39dac9f
serhiy-petryk <sergei_petrik@yahoo.com>
2026-05-16 05:23:09 +0300

    added new MLNETTaxi project for .Net 5.0

--- PredictStockPrice/MLNETTaxi/Program.cs
+++ PredictStockPrice/MLNETTaxi/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using Microsoft.ML;
+using Microsoft.ML.Transforms;
 using MLNETTaxi.Models;
 
 namespace MLNETTaxi
@@ -38,7 +39,7 @@ namespace MLNETTaxi
                 // Choose a learning algorithm
                 .Append(mlContext.Regression.Trainers.FastTree());
 
-            var x1 = mlContext.Transforms.CopyColumns(outputColumnName: "Label", inputColumnName: "FareAmount");
+            ColumnCopyingEstimator x1 = mlContext.Transforms.CopyColumns(outputColumnName: "Label", inputColumnName: "FareAmount");
             var x2 = x1.Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "VendorIdEncoded",
                 inputColumnName: "VendorId"));
             var x3 = x2.Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "RateCodeEncoded",
---
+++ PredictStockPrice/MLNETTaxiNet/DataStructures/TaxiTripFarePrediction.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Data;
+
+namespace MLNETTaxiNet.DataStructures
+{
+    public class TaxiTripFarePrediction
+    {
+        [ColumnName("Score")]
+        public float FareAmount;
+    }
+}
